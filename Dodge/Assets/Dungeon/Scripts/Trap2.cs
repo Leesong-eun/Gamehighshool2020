0194590bc0a1f881b1d3483f250368678d1a8d73
leesongeun <song642100@naver.com>
2020-07-09 21:15:20 +0900

    던전 2일차

--- Dodge/Assets/Dungeon/Scripts/Trap2.cs
+++ Dodge/Assets/Dungeon/Scripts/Trap2.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Trap2 : MonoBehaviour
 {
-
+    public UnityEvent m_OnEnter;
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("트리거 안에 어떤 Collider나 Trigger가 들어갔을 때");
@@ -12,14 +13,19 @@ public class Trap2 : MonoBehaviour
         {
             var player = other.attachedRigidbody.GetComponent<PlayerController_Dungeon2>();
             if (player != null)
-                player.Die();
+                m_OnEnter.Invoke();
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
         Debug.Log("트리거 안에 어떤 Collider나 Trigger가 나왔을 때");
-        //스폰
+        if(other.attachedRigidbody != null)
+        {
+            var player = other.attachedRigidbody.GetComponent<PlayerController_Dungeon2>();
+            if (player != null)
+                m_OnEnter.Invoke();
+        }
     }
 
     private void OnTriggerStay(Collider other)
--- Dodge/Assets/Dungeon/Scripts/Trap3.cs
+++ Dodge/Assets/Dungeon/Scripts/Trap3.cs
@@ -1,12 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
+
+
 
 public class Trap3 : MonoBehaviour
 {
+    public UnityEvent m_OnExit;
     private void OnCollisionExit(Collision collision)
     {
         Debug.Log("Collision이 어떤 Collision과 충돌이 끝났을 때");
+        if (collision.rigidbody != null)
+        {
+            var player = collision.rigidbody
+                .GetComponent<PlayerController_Dungeon>();
+            if (player != null)
+                m_OnExit.Invoke();
+        }
     }
 
     private void OnCollisionStay(Collision collision)
