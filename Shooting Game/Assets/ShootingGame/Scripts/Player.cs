00551ff2ed49ce27c3723dc9de24396bb84e9de6
leesongeun <song642100@naver.com>
2020-07-20 21:06:53 +0900

    슈팅겍임 1일차

---
+++ Shooting Game/Assets/ShootingGame/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+            transform.position += new Vector3(0, 6, 0) * Time.deltaTime;
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.position -= new Vector3(0, 6, 0) * Time.deltaTime;
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.position += new Vector3(6, 0, 0) * Time.deltaTime;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.position -= new Vector3(6, 0, 0) * Time.deltaTime;
+
+
+    }
+}
