b7d66e64e3fbe353999821ad3ab9513cdab46872
leesongeun <song642100@naver.com>
2020-07-23 21:21:26 +0900

    애니메이션 테스트 1일차

---
+++ AnimationTest/Assets/CosmicMan/DemoScripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float Delay = 3f;
+    public float CoolTime = 0f;
+
+    private void Update()
+    {
+        //if(CoolTime >= Delay)
+        //    GetComponent<An>
+    }
+}
